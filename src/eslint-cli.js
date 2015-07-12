3367aaee19f806966ea040f0d1e3115f0678ccee
Colten Rouska <crouska@gmail.com>
2015-07-11 21:12:46 -0600

    Attempting to fix travis node 0.10 build #4

--- src/eslint-cli.js
+++ src/eslint-cli.js
@@ -2,9 +2,9 @@
 var child = require('child-process-promise');
 var path = require('path');
 var os = require('os');
+
 var cmd = os.platform() === 'win32' ? '.cmd' : '';
 var eslint = path.resolve('./node_modules/.bin/eslint' + cmd);
-
 var exec = child.spawn;
 
 module.exports = function(args){

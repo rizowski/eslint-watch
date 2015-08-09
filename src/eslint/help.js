c8914a7ccbf8a9c2b94a91a0988bb59d1aa8bd7d
Colten Rouska <crouska@leisurelink.com>
2015-08-09 11:09:14 -0600

    Got basic option logic to return a new option object off of eslint help

--- src/eslint/help.js
+++ src/eslint/help.js
@@ -12,25 +12,53 @@ var consts = {
   format: 4,
   watch: 5
 }
+/*
+{
+  option: 'help',
+  alias: 'h',
+  type: 'Boolean',
+  description: 'Show help'
+}
+*/
+function createOption(str){
+  var arr = str.trim().split(' ');
+  var result = arr[0].match(/--\w/);
+  var option;
+  if(result){
+    option = parseRegular(arr);
+  } else {
+    option = parseAlias(arr);
+  }
+  return option;
+}
 
-function parseHelp(helpText, eswHelp){
-  var eswHelpArr = eswHelp.split('\n');
-  console.log(eswHelpArr)
+function parseAlias(arr){
+  var option = parseRegular(_.without(arr, arr[0]));
+  option.alias = arr[0];
+  return option;
+}
+
+function parseRegular(arr){
+  var option = {};
+  option.option = arr[0];
+  option.type = arr[1];
+  var helpText = _.without(arr, arr[0], arr[1], '');
+
+  option.description = helpText.join(' ');
+
+  return option;
+}
+
+function parseHelp(helpText){
   var helpArr = helpText.split('\n');
   var newArr = [];
   var result = _.each(helpArr, function(row, index){
-    if(index === 0){
-      newArr.push(eswHelpArr[consts.cmd]);
-    } else{
-      if(contains(row, '--help')){
-        newArr.push(eswHelpArr[consts.help]);
-      } else if(contains(row, '--format')){
-        newArr.push(eswHelpArr[consts.format])
-      } else{
-        newArr.push(row);
-      }
+    if(index === 0 || index === 1 || index === 2){
+      return;
+    } else {
+      console.log(createOption(row));
+      newArr.push(createOption(row));
     }
-
   });
   return newArr;
 }
@@ -40,7 +68,7 @@ module.exports = function(options, parsedOptions){
   var spawn = eslint(['--help'], parsedOptions, {});
   spawn.stdout.on('data', function(msg){
     var eslintHelp = msg.toString();
-    console.log(parseHelp(eslintHelp, eswHelp));
+    parseHelp(eslintHelp, eswHelp);
     // console.log();
     // console.log(result);
   });

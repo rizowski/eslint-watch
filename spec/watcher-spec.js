5dc37445b5eca4defb10caf1a015d71d6ad2fc91
Calvin Woo <calvinwoo32@gmail.com>
2015-08-18 16:00:10 -0700

    Watching works with multiple paths and extensions
    Fixes #19.
    Watch module now covered with unit tests.

---
+++ spec/watcher-spec.js
@@ -0,0 +1,83 @@
+'use strict';
+var chai = require('chai');
+var sinon = require('sinon');
+var sinonChai = require('sinon-chai');
+
+chai.use(sinonChai);
+
+var expect = chai.expect;
+var watcher = require('../src/watcher');
+var chokidar = require('chokidar');
+
+describe('Watcher', function () {
+  var watchStub;
+  var watchOnSpy;
+  var watchAddSpy;
+
+  beforeEach(function () {
+    watchOnSpy = sinon.spy(function() {
+      // No operations
+    });
+
+    watchAddSpy = sinon.spy(function() {
+      // No operations
+    });
+
+    watchStub = sinon.stub(chokidar, 'watch', function() {
+      return {
+        on: watchOnSpy,
+        add: watchAddSpy
+      };
+    });
+  });
+
+  afterEach(function () {
+    chokidar.watch.restore();
+  });
+
+  it('should call the chokidar on function to activate watching', function () {
+    watcher({
+      _: './'
+    });
+
+    expect(watchOnSpy.calledOnce).to.be.true;
+  });
+
+  it('should use the correct default extension and directory', function () {
+    watcher({
+      _: './'
+    });
+
+    expect(watchAddSpy.calledWith('+()**/*+(.js)$')).to.be.true;
+  });
+
+  it('should use the correct watch pattern when a directory is specified', function () {
+    watcher({
+      _: ['directory']
+    });
+
+    expect(watchAddSpy.calledOnce).to.be.true;
+    expect(watchAddSpy.calledWith('+(directory)**/*+(.js)$')).to.be.true;
+  });
+
+  it('should use the correct watch pattern when a file and extension are specified', function () {
+    watcher({
+      _: ['file.test'],
+      ext: ['.test']
+    });
+
+    expect(watchAddSpy.calledOnce).to.be.true;
+    expect(watchAddSpy.calledWith(['file.test'])).to.be.true;
+  });
+
+  it('should use the correct watch pattern when a complex set of files, directories, and extensions are specified', function () {
+    watcher({
+      _: ['file.test', 'file.js', 'directory1', 'directory2'],
+      ext: ['.js', '.test']
+    });
+
+    expect(watchAddSpy.calledTwice).to.be.true;
+    expect(watchAddSpy.calledWith(['file.test', 'file.js']));
+    expect(watchAddSpy.calledWith('+(directory1|directory2)**/*+(.js|.test)$'));
+  });
+});
--- src/watcher.js
+++ src/watcher.js
@@ -7,38 +7,92 @@ var _ = require('lodash');
 var success = require('./formatters/helpers/success');
 var formatter = require('./formatters/simple-detail');
 
-var defaultPath = './';
-var jsFileRules = [];
+// Use an empty string instead of "./" for glob pattern purposes
+var defaultPaths = [''];
+var defaultExtensions = ['.js'];
 var events = {
   change: 'change'
 };
 
 var cli = new eslint.CLIEngine();
 
-function watcher(options) {
-  var specifiedPath = options._;
-  var pathToWatch = specifiedPath.length ? specifiedPath : defaultPath;
+/**
+ * Given an array of paths and extensions, figure out which paths refer to files.
+ * A file is identified by a path ending in ".ext", where ".ext" is one of the extensions that is provided.
+ */
+function findFilePaths(paths, extensions) {
+  var extensionRegex = new RegExp('^.*(' + extensions.join('|') + ')$');
 
-  _.each(options.ext, function(extension){
-    jsFileRules.push('**/*' + extension + '$');
+  return _.filter(paths, function (path) {
+    return path.search(extensionRegex) > -1;
   });
+}
 
-  var watch = chokidar.watch(pathToWatch + jsFileRules);
-
-  console.log('Watching', pathToWatch, '\n');
-
-  function successMessage(result) {
-    if (result.errorCount === 0 && result.warningCount === 0) {
-      return success(result) + chalk.grey(' (' + new Date().toLocaleTimeString() + ')');
-    }
-    return '';
+// Add files and paths to the watcher
+function watchPaths(watch, filePaths, directoryPaths, extensions) {
+  if (filePaths.length > 0) {
+    watch.add(filePaths);
   }
 
-  function lintFile(path, config) {
-    var results = cli.executeOnFiles([path], config).results;
-    console.log(successMessage(results[0]));
-    console.log(formatter(results));
+  // For example:
+  // +(directory1|directory2)**/*+(.ext1|.ext2)
+  if (directoryPaths.length > 0) {
+    watch.add('+(' + directoryPaths.join('|') + ')**/*+(' + extensions.join('|') + ')$');
   }
+};
+
+function successMessage(result) {
+  if (result.errorCount === 0 && result.warningCount === 0) {
+    return success(result) + chalk.grey(' (' + new Date().toLocaleTimeString() + ')');
+  }
+  return '';
+}
+
+function lintFile(path, config) {
+  var results = cli.executeOnFiles([path], config).results;
+  console.log(successMessage(results[0]));
+  console.log(formatter(results));
+}
+
+function watcher(options) {
+  var specifiedPaths;
+  var directoryPaths;
+  var filePaths;
+  var extensions;
+
+  /**
+   * If the user has specified particular paths, the _ property will contain an array.
+   * Otherwise, the _ property will contain "./", the default as a string.
+   * In the default case, use an array instead.
+   */
+  if (options._ instanceof Array) {
+    specifiedPaths = options._;
+  } else {
+    specifiedPaths = defaultPaths;
+  }
+
+  /**
+   * If the user has specified extensions, those will be passed in as an array,
+   * otherwise, options.ext won't be defined.
+   */
+  if (options.ext && options.ext.length > 0) {
+    extensions = options.ext;
+  } else {
+    extensions = defaultExtensions;
+  }
+
+  /**
+   * Files and directories require different glob patterns.
+   * Split the up the paths into 2 separate arrays.
+   * What is not a file is considered a directory.
+   */
+  filePaths = findFilePaths(specifiedPaths, extensions);
+  directoryPaths = _.difference(specifiedPaths, filePaths);
+
+  var watch = chokidar.watch();
+  watchPaths(watch, filePaths, directoryPaths, extensions);
+
+  console.log('Watching', options._, '\n');
 
   watch.on(events.change, function (path) {
     if(!cli.isPathIgnored(path)){

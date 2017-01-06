1bddd724e50152fe32f478ed9f056d5492156cce
Colten Rouska <Colten.Rouska@extendhealth.com>
2017-01-06 13:14:31 -0700

    Making fixes to tests

--- tests/integration/integration-spec.js
+++ tests/integration/integration-spec.js
@@ -71,7 +71,7 @@ describe('integration', function(){
     it('finds 5 issues in test-files', function(){
       var output = esw('--no-ignore "' + testFiles + '"');
       expect(output.error).to.be.true;
-      expect(output.message).to.have.string('7 errors');
+      expect(output.message).to.have.string('5 errors');
     });
 
     it('finds 2 warnings', function(){

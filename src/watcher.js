741a6f40d2a75438851a32334e8707c8d7151b42
Colten Rouska <Colten.Rouska@extendhealth.com>
2017-02-10 14:51:13 -0700

    Implemented #29

--- src/watcher.js
+++ src/watcher.js
@@ -1,45 +1,36 @@
-var chokidar = require('chokidar');
-var eslint = require('eslint');
-var chalk = require('chalk');
-var _ = require('lodash');
-var path = require('path');
+import chokidar from 'chokidar';
+import eslint from 'eslint';
+import _ from 'lodash';
+import path from 'path';
 
-var success = require('./formatters/helpers/success');
-var formatter = require('./formatters/simple-detail');
-var logger = require('./log')('watcher');
+import formatter from './formatters/simple-detail';
+import Logger from './log';
+
+const logger = Logger('watcher');
+
 logger.debug('Loaded');
 
-var events = {
-  change: 'change'
-};
-var chokidarOptions = {
+const events = { change: 'change' };
+const chokidarOptions = {
   ignored: /\.git|node_modules|bower_components/
 };
-var cliOptionProperties = [
+const cliOptionProperties = [
   'config', 'eslintrc', 'ext',
   'parser', 'cache', 'cacheLocation',
   'ignore', 'ignorePath', 'ignorePattern',
   'fix', 'parserOptions', 'global'
 ];
-var cliOptionMap = {
+const cliOptionMap = {
   config: 'configFile',
   eslintrc: 'useEslintrc',
   ext: 'extensions',
   cacheFile: 'cacheLocation'
 };
 
-function successMessage(result) {
-  logger.debug('result: %o', result);
-  if (!result.errorCount && !result.warningCount) {
-    return success(result) + chalk.grey(' (' + new Date().toLocaleTimeString() + ')');
-  }
-  return '';
-}
-
 ///https://github.com/eslint/eslint/blob/233440e524aa41545b66b2c3c7ca26fe790e32e0/tests/lib/cli-engine.js#L105-L107
 
-module.exports = function watcher(options) {
-  var cliOptions = _(options)
+export default function watcher(options) {
+  let cliOptions = _(options)
     .pick(cliOptionProperties)
     .reduce(function(result, value, key){
       key = cliOptionMap[key] || key;
@@ -48,16 +39,16 @@ module.exports = function watcher(options) {
     }, {});
   logger.debug(cliOptions);
   logger.debug(options);
-  var cli = new eslint.CLIEngine(cliOptions);
+  let cli = new eslint.CLIEngine(cliOptions);
 
   function lintFile(path) {
     logger.debug('lintFile: %s', path);
-    var report = cli.executeOnFiles([path]);
+    let report = cli.executeOnFiles(path);
     if (options.fix) {
       eslint.CLIEngine.outputFixes(report);
     }
-    var results = report.results;
-    logger.log(successMessage(results[0]));
+
+    let results = report.results;
     logger.log(formatter(results));
   }
 
@@ -70,12 +61,14 @@ module.exports = function watcher(options) {
     // Use the ESLint default extension, if none is provided
     return _.includes(cli.options.extensions, path.extname(filePath));
   }
-  var watchDir = options._.length ? options._ : [path.resolve('./')];
+  let watchDir = options._.length ? options._ : [path.resolve('./')];
+
   chokidar.watch(watchDir, chokidarOptions)
-    .on(events.change, function (path) {
+    .on(events.change, function changeEvent(path) {
       logger.debug('Changed:', path);
       if (!cli.isPathIgnored(path) && isWatchableExtension(path, options.ext)) {
-        lintFile(path);
+        const watchPath = options.fullLint ? watchDir : [path];
+        lintFile(watchPath);
       }
     }).on('error', logger.error);
 
